dda061bed684e94d9e2bd5b358e1154326a6afcf
johanduran <johan.durancerdas@gmail.com>
2017-07-08 16:28:33 -0600

    test db

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
-	require 'views/index.view.php'
 	$mbd = new PDO('mysql:host=localhost;dbname=ingplantaeDB', 'johanduran', '');
     foreach($mbd->query('SELECT * from variables') as $fila) {
         print_r($fila);
     }	
+	require 'views/index.view.php'
 ?>
